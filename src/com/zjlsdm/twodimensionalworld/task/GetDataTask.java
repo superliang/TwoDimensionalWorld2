6621eeb79339cec893f33585de3657291fa720c8
superliang <super_liang@sina.cn>
2014-07-29 17:32:35 +0800

    实现了获取数据的节点的方法
    实现了获取数据的节点的方法

--- src/com/zjlsdm/twodimensionalworld/task/GetDataTask.java
+++ src/com/zjlsdm/twodimensionalworld/task/GetDataTask.java
@@ -1,43 +1,84 @@
 package com.zjlsdm.twodimensionalworld.task;
 
-import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.htmlparser.Node;
+import org.htmlparser.NodeFilter;
 import org.htmlparser.Parser;
-import org.htmlparser.util.NodeIterator;
+import org.htmlparser.util.NodeList;
 
 import android.os.AsyncTask;
 import android.util.Log;
 
-import com.zjlsdm.twodimensionalworld.util.NetTool;
-
-public class GetDataTask extends AsyncTask<String, String, String>{
+public class GetDataTask extends AsyncTask<String, String, String> {
 	private String dataUrl;
-	
-	public GetDataTask(String dataUrl){
+
+	public GetDataTask(String dataUrl) {
 		this.dataUrl = dataUrl;
 	}
 
 	@Override
 	protected String doInBackground(String... params) {
+		final List<String> strList = new ArrayList<String>();
 		try {
-			String getHtml = NetTool.sendTxt(dataUrl);
-			Parser parser = new Parser(((HttpURLConnection)(new URL(dataUrl)).openConnection()));
-			for(NodeIterator i = parser.elements();i.hasMoreNodes();){
-				Node node = i.nextNode();
-				Log.e("getText", node.getText());
-				Log.e("getPlianText", node.toPlainTextString());
-				Log.e("toHtml", node.toHtml());
-				Log.e("toString", node.toString());
-			}
-			Log.e("++++", getHtml);
+			URL url = new URL(dataUrl);
+			URLConnection urlConnection = url.openConnection();
+			Parser parser = new Parser(urlConnection);
+			parser.setEncoding("UTF-8");
+			NodeList nodeList = parser.parse(new NodeFilter() {
+				/**
+				 * 
+				 */
+				private static final long serialVersionUID = 1L;
+
+				@Override
+				public boolean accept(Node node) {
+					Node need = node;
+					if(getStringsByRegex(node.getText())){
+						strList.add(need.toPlainTextString());
+						return true;
+					}
+					return false;
+				}
+			});
+			// for(NodeIterator i = parser.elements();i.hasMoreNodes();){
+			// Node node = i.nextNode();
+			// Log.e("getText", node.getText());
+			// Log.e("getPlianText", node.toPlainTextString());
+			// Log.e("toHtml", node.toHtml());
+			// Log.e("toString", node.toString());
+			// }
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
+		for(String temp:strList){
+			Log.e("++++++++",temp);
+		}
+		
 		return null;
 	}
-	
-	
+
+	private static boolean getStringsByRegex(String txt) {
+		String regex = "span class=\"none\" acgdb-data-type=\"summary\"";
+
+		Pattern p = Pattern.compile(regex);
+
+		Matcher m = p.matcher(txt);
+
+		if (m.find()) {
+
+			return true;
+
+		}
+
+		return false;
+
+	}
 
 }
